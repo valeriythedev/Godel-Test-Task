d23685b19fa4a868f173f15b790577618411bf8c
ValeriyLiashuk <valeriyliashuk@gmail.com>
2022-01-05 22:19:20 +0300

    create unit test for Service layer and update DTO model

--- src/main/java/com/mastery/task/dto/EmployeeDTO.java
+++ src/main/java/com/mastery/task/dto/EmployeeDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +19,12 @@ public class EmployeeDTO {
     private String jobTitle;
     private Date dateOfBirth;
     private Integer genderId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeDTO that = (EmployeeDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(departmentId, that.departmentId) && Objects.equals(jobTitle, that.jobTitle) && Objects.equals(dateOfBirth, that.dateOfBirth) && Objects.equals(genderId, that.genderId);
+    }
 }
