4ca30ee27603ce6d4fa6a2fbd491d10c1a742449
ValeriyLiashuk <valeriyliashuk@gmail.com>
2022-01-06 16:04:31 +0300

    update pom.xml, and create Swagger documentation

--- src/main/java/com/mastery/task/dto/EmployeeDTO.java
+++ src/main/java/com/mastery/task/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.mastery.task.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +11,27 @@ import java.util.Objects;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Entity of EmployeeDTO")
 public class EmployeeDTO {
 
     private Integer id;
+
+    @Schema(description = "Alex", example = "Alex")
     private String firstName;
+
+    @Schema(description = "Johnson", example = "Johnson")
     private String lastName;
+
+    @Schema(description = "1- Godel, 2 - EPAM, 3 - ISSoft, 4 - ITechArt, 5 - Wargaming", example = "1")
     private Integer departmentId;
+
+    @Schema(description = "Junior Java Developer", example = "Junior Java Developer")
     private String jobTitle;
+
+    @Schema(description = "2003-02-11", example = "2003-02-11")
     private Date dateOfBirth;
+
+    @Schema(description = "1 - MALE, 2 - FEMALE", example = "1")
     private Integer genderId;
 
     @Override
--- src/main/java/com/mastery/task/rest/EmployeeController.java
+++ src/main/java/com/mastery/task/rest/EmployeeController.java
@@ -3,6 +3,9 @@ package com.mastery.task.rest;
 import com.mastery.task.dto.EmployeeDTO;
 import com.mastery.task.model.Employee;
 import com.mastery.task.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +25,8 @@ import java.util.List;
 @RequestMapping(value = "/api/employees/",
 produces = MediaType.APPLICATION_JSON_VALUE,
 consumes = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Employee Controller"
+        , description = "Interaction with employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -33,31 +38,37 @@ public class EmployeeController {
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping
-    public EmployeeDTO create(@RequestBody EmployeeDTO employeeDTO) {
+    @Operation(summary = "Creating employee")
+    public EmployeeDTO create(@RequestBody @Parameter(description = "Employee body") EmployeeDTO employeeDTO) {
         employeeService.create(employeeDTO);
         return employeeDTO;
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{id}")
-    public Employee update(@RequestBody Employee employee, @PathVariable("id") Integer id) {
+    @Operation(summary = "Updating employee")
+    public Employee update(@RequestBody Employee employee,
+                           @PathVariable("id") @Parameter(description = "Id of updating employee") Integer id) {
         return employeeService.update(employee, id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
-    public Integer delete(@PathVariable("id") Integer id) {
+    @Operation(summary = "Deleting employee")
+    public Integer delete(@PathVariable("id") @Parameter(description = "Id of deleting employee") Integer id) {
         return employeeService.delete(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public EmployeeDTO getById(@PathVariable("id") Integer id) {
+    @Operation(summary = "Getting employee")
+    public EmployeeDTO getById(@PathVariable("id") @Parameter(description = "Id of getting employee") Integer id) {
         return employeeService.getById(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
+    @Operation(summary = "Get all employees")
     public List<EmployeeDTO> getAll() {
         return employeeService.getAll();
     }
